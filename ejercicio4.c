2b5b108c8cd494b77e70552192ba59bd34b25d49
CarlosVargas16 <carvargasr16@gmail.com>
2025-10-28 02:16:34 -0600

    Create ejercicio4.c

---
+++ ejercicio4.c
@@ -0,0 +1,17 @@
+/* Vargas Rodriguez Carlos Antonio
+ejercicio 4*/
+
+#include <stdio.h>
+
+void main ()
+{
+  int i;
+  
+  for (int i=1; i<=100; i++)
+  {
+    if (i%3==0)
+    {
+    printf ("num: %d\n", i);
+    }
+  }
+}
